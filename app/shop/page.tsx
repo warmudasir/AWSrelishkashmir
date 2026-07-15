ecebf418e5375d35d03d0d53fd9561e53f808fe7
saabu <warmudasir095@gmail.com>
2026-07-15 12:49:24 +0530

    fix

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Products from "../components/products";
+import { Product } from "../types/common";
+import { getProducts } from "../services/products/getproducts";
 
-export default function page() {
+export default async function page() {
+  const products: Product[] = await getProducts();
   return (
     <div>
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
